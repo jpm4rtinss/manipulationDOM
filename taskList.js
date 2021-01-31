5d308f7cf26fe06867e3ec418e41001e7dc5d888
João Pedro Martins <joaopedromsn@gmail.com>
2021-01-31 01:47:12 -0300

    delete Task finished

--- taskList.js
+++ taskList.js
@@ -13,13 +13,14 @@
     listElement.appendChild(taskElement)
 
     taskElement.appendChild(doneButtonFunc())
+    taskElement.appendChild(deleteButtonFunc())
 
     input.value = ''
 }
 
 const doneButtonFunc = ()=>{
     const doneButton = document.createElement('button')
-    doneButton.classList.add('check')
+    doneButton.classList.add('check-button')
     doneButton.innerText = 'Concluido'
     doneButton.addEventListener('click', completeTaskFunc)
 
@@ -39,14 +40,20 @@ buttonNewTask.addEventListener('click', createTaskFunc)
 const deleteButtonFunc = ()=>{
     const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Deletar'
-    deleteButton.addEventListener('click', ()=>{
-        console.log('deletado')
-        
-    })
+    deleteButton.classList.add('delete-button')
+    deleteButton.addEventListener('click', deleteTask)
     return deleteButton
     
 
 }
+
+
+const deleteTask = (event)=>{
+    const deleteButton = event.target
+    const taskLi= deleteButton.parentElement
+    taskLi.remove()
+}
+
 })()
     
 
